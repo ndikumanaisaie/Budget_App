35f13adb187d6c7bbae1e01e3c9cbb4056af2401
ndikumana isaie <ndikumanaisaie@gmail.com>
2023-03-21 19:39:36 +0200

    Add migration files

--- db/migrate/20230321150046_add_devise_to_users.rb
+++ db/migrate/20230321150046_add_devise_to_users.rb
@@ -4,8 +4,8 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
   def self.up
     change_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :name                null: false, default: ""
+      # t.string :email,              null: false, default: ""
+      # t.string :name,                null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -35,7 +35,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
 
 
       # Uncomment below if timestamps were not included in your original model.
-      t.timestamps null: false
+      # t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true

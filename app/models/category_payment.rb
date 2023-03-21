97cf1c7fcbab78b9e58a2384354945b44356f0e7
ndikumana isaie <ndikumanaisaie@gmail.com>
2023-03-21 16:36:15 +0200

    Add associations to category_payment

--- app/models/category_payment.rb
+++ app/models/category_payment.rb
@@ -1,4 +1,7 @@
 class CategoryPayment < ApplicationRecord
-  belongs_to :category
-  belongs_to :payment
+  belongs_to :category, class_name: 'Category'
+  belongs_to :payment, class_name: 'Payment'
+
+  validates :category_id, presence: true
+  validates :payment_id, presence: true
 end

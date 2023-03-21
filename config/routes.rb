1aa7090f1cbe2fce5cb621f2057444fe7787552c
ndikumana isaie <ndikumanaisaie@gmail.com>
2023-03-21 17:03:42 +0200

    Add proper routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :category_payments
-  resources :payments
-  resources :categories
-  resources :users
+  devise_scope :user do
+    authenticated :user do
+      root 'categories#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'users#index', as: :unauthenticated_root
+    end
+  end
+
+  get 'categories/most_recent_list'
+  get 'categories/most_ancient_list'
+
+  resources :categories, only: %i[index new create show destroy update] do
+    resources :payments, only: %i[index new create destroy update]
+  end
+
+  resources :users, only: %i[index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

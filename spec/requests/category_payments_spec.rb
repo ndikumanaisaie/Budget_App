4a68ba7289ca4ed949c897edce7f92f21ff942c4
ndikumana isaie <ndikumanaisaie@gmail.com>
2023-03-22 00:10:56 +0200

    Fix empty line offense

--- spec/requests/category_payments_spec.rb
+++ spec/requests/category_payments_spec.rb
@@ -76,7 +76,6 @@ RSpec.describe '/category_payments', type: :request do
         end.to change(CategoryPayment, :count).by(0)
       end
 
-
       it "renders a response with 422 status (i.e. to display the 'new' template)" do
         post category_payments_url, params: { category_payment: invalid_attributes }
         expect(response).to have_http_status(:unprocessable_entity)

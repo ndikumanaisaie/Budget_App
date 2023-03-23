f303d4b193ce42969f3e6da942cf7dc30382558f
ndikumana isaie <ndikumanaisaie@gmail.com>
2023-03-23 16:22:17 +0200

    Add model tests

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,24 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
-  let(:user) { User.create(name: 'user', email: 'example@mail.com', password: 'password') }
-  let(:category) { Category.create(name: 'food', icon: 'icon.png', user_id: user.id) }
- 
+  before(:all) do
+    User.destroy_all
+    Category.destroy_all
+    Payment.destroy_all
 
-  subject do
-    described_class.new(name: 'name', amount: 20, user_id: user.id,
-                        category_id: category.id)
+    @user = create(:user)
+    @category = create(:category, user: @user)
+    @payment = create(:payment, user: @user, category_id: @category.id)
   end
 
-  describe 'Validations' do
+  describe 'Payment validations' do
+    it 'is valid with valid attributes' do
+      expect(@payment).to be_valid
+    end
+
     it 'is not valid without a name' do
-      subject.name = nil
-      expect(subject).to_not be_valid
+      payment = build(:payment, name: nil)
+      expect(payment).to_not be_valid
+    end
+
+    it 'is not valid without a user' do
+      payment = build(:payment, user: nil)
+      expect(payment).to_not be_valid
     end
 
     it 'is not valid without an amount' do
-      subject.amount = nil
-      expect(subject).to_not be_valid
+      payment = build(:payment, amount: nil)
+      expect(payment).to_not be_valid
     end
   end
 end

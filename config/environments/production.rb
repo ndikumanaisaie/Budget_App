b954fcfb9b1abc34ebea8982c58185901025de41
ndikumana isaie <ndikumanaisaie@gmail.com>
2023-03-24 00:16:04 +0200

    Remove erronous file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,8 +30,6 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
-  Sass::Plugin.options[:never_update] = true
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
